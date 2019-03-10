45cd62cc8f038c28a3b9e5ef01bd943613be467d
Liam Moat <liam.moat@microsoft.com>
2019-03-10 15:39:01 +0000

    feat(preview): preview a generated .gitignore file

--- src/LiamMoat.DotNet.GitIgnore/App.cs
+++ src/LiamMoat.DotNet.GitIgnore/App.cs
@@ -3,11 +3,16 @@ using McMaster.Extensions.CommandLineUtils;
 
 namespace LiamMoat.DotNet.GitIgnore
 {
-	[HelpOption]
-	[Subcommand(typeof(ListCommand))]
-	[Subcommand(typeof(GenerateCommand))]
-	public class App
-	{
-
-	}
+    [HelpOption]
+    [Subcommand(typeof(ListCommand))]
+    [Subcommand(typeof(GenerateCommand))]
+    [Subcommand(typeof(PreviewCommand))]
+    public class App
+    {
+        private int OnExecute(CommandLineApplication app, IConsole console)
+        {
+            app.ShowHelp();
+            return 0;
+        }
+    }
 }
---
+++ src/LiamMoat.DotNet.GitIgnore/Commands/PreviewCommand.cs
@@ -0,0 +1,32 @@
+﻿using LiamMoat.DotNet.GitIgnore.Services;
+using McMaster.Extensions.CommandLineUtils;
+using System.Threading.Tasks;
+
+namespace LiamMoat.DotNet.GitIgnore.Commands
+{
+    [HelpOption]
+	[Command(Description = "Preview a generated a .gitignore file")]
+	public class PreviewCommand
+	{
+		private readonly IGitIgnoreService _gitIgnoreService;
+
+        public PreviewCommand(IGitIgnoreService gitIgnoreService)
+		{
+			_gitIgnoreService = gitIgnoreService;
+        }
+
+		[Argument(0, "Types", "")]
+		public string[] Types { get; set; }
+
+		private async Task<int> OnExecuteAsync(CommandLineApplication app, IConsole console)
+		{
+			console.WriteLine($"Previewing a .gitignore file for {string.Join(',', Types)}");
+
+			var contents = await _gitIgnoreService.GetTemplateAsync(Types);
+
+            console.WriteLine(contents);
+
+			return 0;
+		}
+	}
+}
